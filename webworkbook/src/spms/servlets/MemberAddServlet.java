c05e79af0c17a4bd0bde6da85b989745f5044321
Kangjin Lee <ranian129@gmail.com>
2015-05-14 17:02:05 +0900

    - 회원 등록 관련해서 뷰를 JSP로 분리시킴.

--- webworkbook/src/spms/servlets/MemberAddServlet.java
+++ webworkbook/src/spms/servlets/MemberAddServlet.java
@@ -6,6 +6,7 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -21,18 +22,9 @@ public class MemberAddServlet extends HttpServlet {
     protected void doGet(HttpServletRequest req, HttpServletResponse resp)
             throws ServletException, IOException {
         resp.setContentType("text/html; charset=UTF-8");
-        PrintWriter out = resp.getWriter();
-        out.println("<html><head><title>회원 등록</title></head>");
-        out.println("<body>");
-        out.println("<h1>회원 등록</h1>");
-        out.println("<form action='add' method='post'>");
-        out.println("    이름: <input type='text' name='name'><br>");
-        out.println("    이메일: <input type='email' name='email'><br>");
-        out.println("    암호: <input type='password' name='password'><br>");
-        out.println("    <input type='submit' value='추가'>");
-        out.println("    <input type='reset' value='취소'><br>");
-        out.println("</form>");
-        out.println("</body></html>");
+        
+        RequestDispatcher rd = req.getRequestDispatcher("/member/MemberAdd.jsp");
+        rd.forward(req, resp);
     }
     
     @Override

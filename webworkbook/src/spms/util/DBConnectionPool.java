e4093a8b5ddbeabe7779d8a0de081e0ac80f705b
kjlee <ranian129@gmail.com>
2015-05-27 00:47:24 +0900

    - DB Connection Pool 버그 수정.  - DB 연결 객체를 get()으로 가져오던 것을 remove()로 가져오도록 함.

--- webworkbook/src/spms/util/DBConnectionPool.java
+++ webworkbook/src/spms/util/DBConnectionPool.java
@@ -12,9 +12,9 @@ public class DBConnectionPool {
 	ArrayList<Connection> connList = new ArrayList<Connection>();
 	
 	public DBConnectionPool(String driver, String url, 
-			String usrename, String password) throws Exception {
+			String username, String password) throws Exception {
 		this.url = url;
-		this.username = usrename;
+		this.username = username;
 		this.password = password;
 		
 		Class.forName(driver);
@@ -22,7 +22,7 @@ public class DBConnectionPool {
 	
 	public Connection getConnection() throws Exception {
 		if (connList.size() > 0) {
-			Connection conn = connList.get(0);
+			Connection conn = connList.remove(0);
 			if (conn.isValid(10)) {
 				return conn;
 			}

f390dc2baa4e1af9d586d2781d17137800a989a5
kjlee <ranian129@gmail.com>
2015-05-29 01:46:58 +0900

    - 애노테이션을 이용한 객체 관리   - 애노테이션 정의 추가: @Component   - Dao, 페이지 컨트롤러에 애노테이션 추가. value에는 변수명을 적는다.   - 프로퍼티 파일 수정(DataSouce만 남겨둔다)   - ApplicationContext에서 애노테이션 처리코드 추가.   - Reflection 라이브러리 추가

--- webworkbook/src/spms/dao/MySqlMemberDao.java
+++ webworkbook/src/spms/dao/MySqlMemberDao.java
@@ -11,8 +11,10 @@ import java.util.List;
 import javax.servlet.ServletException;
 import javax.sql.DataSource;
 
+import spms.annotation.Component;
 import spms.vo.Member;
 
+@Component("memberDao")
 public class MySqlMemberDao implements MemberDao {
 	DataSource ds;
 	
